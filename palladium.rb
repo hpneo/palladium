cffbe3148898fa43b4b299f3afb7347a7d2df705
HPNeo <hpneo@hotmail.com>
2014-05-16 21:59:23 -0500

    Fixed

--- palladium.rb
+++ palladium.rb
@@ -11,39 +11,41 @@ def parse_path(path)
   }
 end
 
-EventMachine.run do
+EM.run do
 
   @channels = {}
 
-  EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 1234) do |ws|
+  EM::WebSocket.start(:host => "0.0.0.0", :port => "1234") do |ws|
     puts "WebSocket server starting"
 
-    ws.onopen do |handshake|
-      puts "Welcome to Palladium"
-      
-      socket_data = parse_path handshake.path
+    socket_data = nil
 
+    ws.onopen do
+      socket_data = parse_path(ws.instance_variable_get('@handler'.to_sym).request["path"])
       @channels[socket_data[:channel]] = EM::Channel.new
+      puts "Welcome to Palladium. Listening: #{socket_data[:channel]}"
 
-      sid = @channels[socket_data[:channel]].subscribe { |msg| ws.send msg }
-
-      ws.onmessage do |msg|
-        message = JSON.parse(msg) rescue {}
-        puts "Receiving a message from #{socket_data[:channel]}:#{socket_data[:event]}"
-
-        response = {
-          :channel => socket_data[:channel],
-          :event => socket_data[:event],
-          :data => msg
-        }
-
-        @channels[socket_data[:channel]].push(response.to_json)
-      end
-
-      ws.onclose do
-        puts "Au revoir!"
+      @channels[socket_data[:channel]].subscribe do |message|
+        ws.send(message)
       end
     end
 
+    ws.onmessage do |message|
+      puts message
+      message = JSON.parse(message) rescue {}
+      puts "Receiving a message from #{socket_data[:channel]}:#{socket_data[:event]}:"
+
+      response = {
+        :channel => socket_data[:channel],
+        :event => socket_data[:event],
+        :data => message
+      }
+
+      @channels[socket_data[:channel]].push(response.to_json)
+    end
+
+    ws.onclose do
+      puts "Au revoir!"
+    end
   end
 end
